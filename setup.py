14cbe620a44747d2e297ffab29b7ad171a0a0329
vagrant <vagrant@precise64.(none)>
2013-10-07 14:23:53 +0000

    updating license/authors

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ f.close()
 setup(
     name='floppy_charcount',
     version=".".join(map(str, VERSION)),
-    description='A django widget that displays a normal text input with a remaining character count beside it.',
+    description='A django fields/widgets that displays a normal text input with a remaining character count beside it.',
     long_description=readme,
     author="Ashley Camba Garrido",
     author_email='ashwoods@gmail.com',
-    url='https://github.com/ashwoods/django-floppy-charcount-widget',
+    url='https://github.com/ashwoods/django-floppy-charcount',
     packages=find_packages(),
-    package_data = {
+    package_data={
         'floppy_charcount': [
         ],
     },
