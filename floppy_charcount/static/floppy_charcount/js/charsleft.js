37b06596c0169f6a72821f8e11d38d82d264e6ee
mathias gutdeutsch <gumabe.at@gmail.com>
2013-10-24 16:47:05 +0200

    fix js counter to display calculated values on pre filled textareas fix some js syntax warnings

--- floppy_charcount/static/floppy_charcount/js/charsleft.js
+++ floppy_charcount/static/floppy_charcount/js/charsleft.js
@@ -1,43 +1,47 @@
 jQuery(function($){
-	
-	$.fn.charsLeft = function(options){
-	
-		var defaults = {	
-			'source':'input',
-			'dest':'.count',
-		}
-		var options = $.extend(defaults, options);
-		
-		var calculate = function(source, dest, maxlength){
-			var remaining = maxlength - source.val().length;
-			dest.html(remaining);
-			/* Over 50%, change colour to orange */
-			p=(100*remaining)/maxlength;
-			if(p<25){
-				dest.addClass('red');
-			}else if(p<59){
-				dest.addClass('orange');
-			}else{
-				dest.removeClass('orange red');
-			}
-		};
-		 	
-		this.each(function(i, el) {
+
+    $.fn.charsLeft = function(options){
+
+        var defaults = {
+            'source':'input',
+            'dest':'.count'
+        };
+        var options = $.extend(defaults, options);
+
+        var calculate = function(source, dest, maxlength){
+            var remaining = maxlength - source.val().length;
+            dest.html(remaining);
+            /* Over 50%, change colour to orange */
+            p=(100*remaining)/maxlength;
+            if(p<25){
+                dest.addClass('red');
+            }else if(p<59){
+                dest.addClass('orange');
+            }else{
+                dest.removeClass('orange red');
+            }
+        };
+
+        this.each(function(i, el) {
 
             var source = $(this).find(options.source);
             var maxlength = source.attr('maxlength');
-			var dest = $(this).find(options.dest);
-			source.keyup(function(){
-				calculate(source, dest, maxlength)
-			});
-			source.change(function(){
-				calculate(source, dest, maxlength)
-			});
-		});
-	};
-	
-	$(".charsleft-input").charsLeft({
-		'source':'textarea, input',
-		'dest':".count"
-	});
+            var dest = $(this).find(options.dest);
+
+            // calculate the remaining chars on first load
+            calculate(source, dest, maxlength);
+
+            source.keyup(function(){
+                calculate(source, dest, maxlength);
+            });
+            source.change(function(){
+                calculate(source, dest, maxlength);
+            });
+        });
+    };
+
+    $(".charsleft-input").charsLeft({
+        'source':'textarea, input',
+        'dest':".count"
+    });
 });
